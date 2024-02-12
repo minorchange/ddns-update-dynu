50b3122cc701f2d4d5981852f59dcd955947e1cd
minorchange <bnu@posteo.org>
2024-02-12 19:52:27 +0100

    Introduce relative config path and improve logging

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 
+# Get the directory where this script resides
+SCRIPT_DIR=$(dirname "$0")
+
 # Read environment variables from file
-CONFIG_FILE="config.txt"
+CONFIG_FILE="$SCRIPT_DIR/config.txt"
+
 if [[ -f "$CONFIG_FILE" ]]; then
     source "$CONFIG_FILE"
 else
@@ -11,12 +15,9 @@ else
 fi
 
 # Set the maximum size for the logfile in bytes 
-MAX_LOG_SIZE_BYTES=10000
+MAX_LOG_SIZE_BYTES=100000
 
-# Get the directory where the script resides
-SCRIPT_DIR=$(dirname "$0")
-
-# Construct the path to dynu.log relative to the script directory
+# Construct the path to dynu.log relative t`o the script directory
 LOG_FILE="$SCRIPT_DIR/update-ddns-dynu-call.log"
 
 # Construct the URL
@@ -39,11 +40,11 @@ echo "Update complete."
 # Check if the logfile exceeds the maximum size
 LOG_SIZE=$(stat -c%s "$LOG_FILE")
 if [[ $LOG_SIZE -gt $MAX_LOG_SIZE_BYTES ]]; then
-    echo "Logfile exceeds maximum size. Truncating..."
+    echo "Logfile exceeds maximum size. Truncating to half the maxium size."
     
-    # Count the number of lines to remove
-    LINES_TO_REMOVE=$((LOG_SIZE - MAX_LOG_SIZE_BYTES))
-
+    # By truncating to half the size the truncation does eventuelly not have to happen each run.
+    LINES_TO_REMOVE=$(($MAX_LOG_SIZE_BYTES / 2))
+    
     # Remove the specified number of lines from the beginning of the file
     tail -c +$((LINES_TO_REMOVE + 1)) "$LOG_FILE" > "$LOG_FILE.tmp" && mv "$LOG_FILE.tmp" "$LOG_FILE"
     
